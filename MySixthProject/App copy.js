3ee270735e79cf2c333f66d315a75b3c89919b73
AhmedAlshehri23 <Ahmed.Alshehri@colostate.edu>
2024-04-28 18:37:33 -0600

    MySixthProject

---
+++ MySixthProject/App copy.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+const culogo = 1 {
+  uri = ""
+width: 80, height: 80
+｝
+
